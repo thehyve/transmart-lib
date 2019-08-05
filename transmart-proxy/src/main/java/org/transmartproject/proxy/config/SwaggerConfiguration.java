7dbfc2a3c398dc88723373085cd49c24514dd243
ewelinagr <ewelina@thehyve.nl>
2019-08-05 09:33:57 +0200

    Configure swagger security to use authorization header

--- transmart-proxy/src/main/java/org/transmartproject/proxy/config/SwaggerConfiguration.java
+++ transmart-proxy/src/main/java/org/transmartproject/proxy/config/SwaggerConfiguration.java
@@ -4,34 +4,59 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+
+    private static final String VERSION = "v0.0.1";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis((RequestHandlerSelectors.basePackage("org.transmartproject.proxy.server")))
             .paths(PathSelectors.any())
             .build()
-            .apiInfo(apiInfo());
+            .apiInfo(apiInfo())
+            .securitySchemes(Arrays.asList(apiKey()))
+            .securityContexts(Arrays.asList(securityContext()));
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
             "TranSMART REST API proxy",
             "Spring Boot application that serves as a TranSMART REST API proxy.",
-            "v0.0.1",
+            VERSION,
             null,
             new Contact("The Hyve", "https://github.com/thehyve/transmart-lib", "office@thehyve.nl"),
-            "MIT", "https://opensource.org/licenses/MIT", Collections.emptyList());
+            "MIT License", "https://opensource.org/licenses/MIT", Collections.emptyList());
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.any())
+            .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Bearer", authorizationScopes));
+    }
+
+    private static ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
     }
 }
